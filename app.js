674e2c7a7c88544176bf6dd38b0f27ced0b79fd4
Josiah Derricott <derricott.joey@hotmail.com>
2019-09-14 15:52:49 -0700

    Update database, add admin page

--- app.js
+++ app.js
@@ -2,19 +2,36 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/nickDB", {useNewUrlParser: true});
+
+const burgerTitle = "THE TROPICAL MANGO BURGER";
+const burgerDesc = "Our 1/4 pound beef patty topped with our home made Fresh Mango Salsa, Mozzarella cheese, and finished off with our home made Mango Mayo, all on a sesame bun! Only for July, only at Nick's Jr! Try one today!!";
 
 app.get("/", function(req, res)
 {
-  res.render("index");
+  res.render("index", {burgTitle: burgerTitle, burgDesc: burgerDesc});
+});
+
+app.get("/admin", function(req, res)
+{
+  res.render("admin", {burgTitle: burgerTitle, burgDesc: burgerDesc});
+});
+
+app.post("/admin", function(req, res)
+{
+  // TODO: update all vars in database here, redirect to home to test
 });
 
 app.listen(process.env.PORT || 3000, function()
 {
-  console.log("Server started on port 3000!");
+  console.log("Server started!");
 });
